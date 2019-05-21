af24517981c7a7828cba31fea58164a7bc842e1d
greenhornsr <srollins11279@gmail.com>
2019-05-21 11:22:17 -0700

    in app.js, added h3 for error display.  in action/index.js, built out fetch axios call.  still missing endpoints.  In reducers/index.js, added case handling for fetch start, success and failure.  pending endpoints.

--- chicago-array/src/App.js
+++ chicago-array/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import './App.css';
 import SignIn from "./components/authorization/SignIn";
 import SignUp from "./components/authorization/SignUp";
+// import './assets/Chicago-AoT.png'
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
-      <h1>It's WORKING!  It's WORKING!</h1>
+      {/* <img src="./assets/Chicago-AoT.png" alt="chicago array of things" /> */}
+      <h1>Sign In or Sign Up</h1>
       <SignUp />
       <SignIn />
+      <section>
+        <h3>{props.error}</h3>
+      </section>
     </div>
   );
 }
 
+const mapStateToProps = state => ({
+  ...state,
+})
+
 // Material-ui
 // DEVELOPMENT BRANCH
 // doc jones
-export default App;
+export default connect(
+  mapStateToProps,
+)(App);
