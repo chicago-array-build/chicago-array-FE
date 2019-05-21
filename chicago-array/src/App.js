f564327b1de1d13048f8e5ccc0a9ca769bff6f3a
greenhornsr <srollins11279@gmail.com>
2019-05-21 13:48:14 -0700

    added navlinks for home, signin, signup

--- chicago-array/src/App.js
+++ chicago-array/src/App.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
+// styles & components
 import './App.css';
 import SignIn from "./components/authorization/SignIn";
 import SignUp from "./components/authorization/SignUp";
+import PrimarySearchAppBar from "./components/ButtonAppBar";
 // import PrivateRoute from './components/PrivateRoute';
+
+// images
 import ChicagoAOT from './assets/Chicago-AoT.png'
 
 
 function App(props) {
   return (
     <div className="App">
+      <section>
+        <PrimarySearchAppBar />
+      </section>
+      <h1>Welcome to the Chicago Array of Things</h1>
       <section className="app-img">
         <img src={ChicagoAOT} alt="chicago array of things" />
       </section>
-      <h1>Sign In or Sign Up</h1>
-      <Route path="/" component={SignUp} />
-      <Route path="/" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={SignIn} />
       {/* <PrivateRoute exact path="/selectors" component={selections} /> */}
       <section>
         <h3>{props.error}</h3>
---
+++ chicago-array/src/components/ButtonAppBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { NavLink } from 'react-router-dom';
+
+// Material UI
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+root: {
+    flexGrow: 1,
+},
+grow: {
+    flexGrow: 1,
+},
+menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+},
+};
+
+function ButtonAppBar(props) {
+const { classes } = props;
+return (
+    <div className={classes.root}>
+    <AppBar position="static">
+        <Toolbar>
+        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" className={classes.grow}>
+            CHICAGO ARRAY OF THINGS
+        </Typography>
+        <NavLink to="/"><Button color="secondary">Home</Button></NavLink>
+        <NavLink to="/signin"><Button color="secondary">Sign In</Button></NavLink>
+        <NavLink to="/signup"><Button color="secondary">Sign Up</Button></NavLink>
+        </Toolbar>
+    </AppBar>
+    </div>
+);
+}
+
+ButtonAppBar.propTypes = {
+classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
