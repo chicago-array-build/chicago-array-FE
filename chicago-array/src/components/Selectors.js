2cb18dc36d625375e587c1899d5e70a320f0d856
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-22 13:37:10 -0400

    Fix git and merge conflicts

--- chicago-array/src/components/Selectors.js
+++ chicago-array/src/components/Selectors.js
@@ -1,44 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Checkbox from '@material-ui/core/Checkbox';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Checkbox from "@material-ui/core/Checkbox";
 
-import { connect } from 'react-redux';
-import { CardActionArea } from '@material-ui/core';
-import { fetchData } from '../actions';
-
+import { connect } from "react-redux";
+import { CardActionArea } from "@material-ui/core";
+import { fetchData } from "../actions";
 
 // Selectors = (event) => {
 //     event.preventDefault()
 //     this.props.selectors(this.state)
 //     this.setState({
-    // sensorType: ['ED AQ IP'],
-    // measure: 'Temp' ,
-    // time: '24',
-    // area: 'Uptown'
+// sensorType: ['ED AQ IP'],
+// measure: 'Temp' ,
+// time: '24',
+// area: 'Uptown'
 //     })
 // }
 
 const styles = theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   formControl: {
-    margin: theme.spacing.unit * 3,
-  },
+    margin: theme.spacing.unit * 3
+  }
 });
 
 class Selectors extends React.Component {
   state = {
-    sensorType: '',
-    measure: '' ,
+    sensorType: "",
+    measure: "",
     time: null,
-    area: ''
+    area: ""
   };
 
   handleChange = sensorType => event => {
@@ -46,8 +45,12 @@ class Selectors extends React.Component {
     this.getMeasures(event.target.name);
   };
 
-  fetchData = event => {event.preventDefault(); this.props.fetchData(this.state)};
+  fetchData = event => {
+    event.preventDefault();
+    this.props.fetchData(this.state);
+  };
 
+<<<<<<< HEAD
   getMeasures = value => {
       const sensorType = props.value;
       if (value === 'ED') {
@@ -57,6 +60,10 @@ class Selectors extends React.Component {
       }else (value === 'IP') {
           return ;
       }
+=======
+  getMeasures = name => {
+    console.log(name);
+>>>>>>> e40eee0936661dfcf8210b41e3ad6c870f2dfa69
   };
 
   render() {
@@ -67,11 +74,22 @@ class Selectors extends React.Component {
     return (
       <div className={classes.root}>
         <FormControl component="fieldset" className={classes.formControl}>
-          <FormLabel component="legend">Select Measures for Data Visualization</FormLabel>
+          <FormLabel component="legend">
+            Select Measures for Data Visualization
+          </FormLabel>
           <FormGroup>
             <FormControlLabel
               control={
+<<<<<<< HEAD
                 <Checkbox name='sensorType' onChange={this.handleChange('sensorType')} value="ED" />
+=======
+                <Checkbox
+                  name="sensorType"
+                  checked={sensorType}
+                  onChange={this.handleChange("sensorType")}
+                  value="ED"
+                />
+>>>>>>> e40eee0936661dfcf8210b41e3ad6c870f2dfa69
               }
               label="Environmental Data"
             />
@@ -90,12 +108,21 @@ class Selectors extends React.Component {
           </FormGroup>
           <FormHelperText>Select one measure per Sensor Type.</FormHelperText>
         </FormControl>
-        <FormControl required error={error} component="fieldset" className={classes.formControl}>
+        <FormControl
+          required
+          error={error}
+          component="fieldset"
+          className={classes.formControl}
+        >
           <FormLabel component="legend">Measures</FormLabel>
           <FormGroup>
             <FormControlLabel
               control={
-                <Checkbox name={this.state.measure} onChange={this.handleChange('temp')} value="Temp" />
+                <Checkbox
+                  name={this.state.measure}
+                  onChange={this.handleChange("temp")}
+                  value="Temp"
+                />
               }
               label={this.state.measure}
             />
@@ -103,7 +130,7 @@ class Selectors extends React.Component {
               control={
                 <Checkbox
                   checked={area}
-                  onChange={this.handleChange('area')}
+                  onChange={this.handleChange("area")}
                   value="Uptown"
                 />
               }
@@ -121,15 +148,16 @@ class Selectors extends React.Component {
 }
 
 Selectors.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    ...state
-})
+  ...state
+});
 
 export default connect(
-    mapStateToProps, 
-    { fetchData },
-) (withStyles(styles)(Selectors));
+  mapStateToProps,
+  { fetchData }
+)(withStyles(styles)(Selectors));
 
+// Comment to commit
--- chicago-array/src/reducers/index.jsx
+++ chicago-array/src/reducers/index.jsx
@@ -1,11 +1,13 @@
-import { SIGNIN_START, SIGNIN_SUCCESS, SIGNIN_FAILURE, SIGNUP_START, SIGNUP_SUCCESS, SIGNUP_FAILURE, FETCH_START, SIGN_OUT } from '../actions';
+import { SIGNIN_START, SIGNIN_SUCCESS, SIGNIN_FAILURE, SIGNUP_START, SIGNUP_SUCCESS, SIGNUP_FAILURE, FETCH_START, FETCH_SUCCESS, FETCH_FAILURE, SIGN_OUT } from '../actions';
 
 const initialState = {
-    caotData: [],
+    caotData: '',
     username: '',
     password: '',
     signingUp: false, 
+    signedUp: false,
     loggingIn: false,
+    message: '',
     isLoggedIn: false,
     fetchingData: false,
     error: '',
@@ -20,11 +22,13 @@ export default function reducer(state = initialState, action){
             ...state, 
             signingUp: true, 
             error: '',
+            message: 'YOU SIGNED UP SUCCESSFULLY!, PLEASE SIGN IN!'
         }
         case SIGNUP_SUCCESS: 
         return {
             ...state,
             signingUp: false,
+            signedUp: true,
         }
         case SIGNUP_FAILURE:
         return {
@@ -38,12 +42,15 @@ export default function reducer(state = initialState, action){
             ...state,
             loggingIn: true,
             error: '',
+            message: 'YOU SIGNED IN SUCESSFULLY!' 
         }
         case SIGNIN_SUCCESS:
         return {
             ...state,
             loggingIn: false, 
             isLoggedIn: true,
+            signedUp: false,
+            message: '',
         }
         case SIGNIN_FAILURE:
         return {
@@ -56,20 +63,22 @@ export default function reducer(state = initialState, action){
         return {
             ...state, 
             fetchingData: true,
+            isLoggedIn: true,
             error: '',
         }
-        // case FETCH_SUCCESS:
-        // return {
-        //     ...state,
-        //     fetchingData: false,
-        //     caotData: action.payload,
-        // }
-        // case FETCH_FAILURE: 
-        // return {
-        //     ...state,
-        //     fetchingData: false,
-        //     error: action.payload
-        // }
+        case FETCH_SUCCESS:
+        return {
+            ...state,
+            fetchingData: false,
+            isLoggedIn: false,
+            caotData: action.payload,
+        }
+        case FETCH_FAILURE: 
+        return {
+            ...state,
+            fetchingData: false,
+            error: action.payload
+        }
         case SIGN_OUT:
         return {
             caotData: [],
